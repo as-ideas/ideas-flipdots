8b066ff4854a82eed6b4ab730c7ea6f355102c44
Sebastian Waschnick <sebastian.waschnick@asideas.de>
2017-03-02 18:11:46 +0100

    WIP

--- app/src/main/java/de/axelspringer/ideas/flipdots/magic/Flipdots.java
+++ app/src/main/java/de/axelspringer/ideas/flipdots/magic/Flipdots.java
@@ -4,7 +4,10 @@ import jssc.SerialPort;
 import jssc.SerialPortException;
 import jssc.SerialPortList;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -35,50 +38,50 @@ import java.util.Map;
  */
 public class Flipdots {
 
-    Map<Character, byte[]> font = new HashMap<>();
+    Map<Character, Byte[]> font = new HashMap<>();
 
     private SerialPort serialPort;
 
     public Flipdots() {
-        font.put(' ', new byte[]{0});
-        font.put('+', new byte[]{24, 126, 126, 24, 0});
-        font.put('-', new byte[]{24, 24, 24, 24, 0});
-        font.put('0', new byte[]{62, 65, 65, 62, 0});
-        font.put('1', new byte[]{0, 66, 127, 64, 0});
-        font.put('2', new byte[]{98, 81, 73, 70, 0});
-        font.put('3', new byte[]{34, 65, 73, 54, 0});
-        font.put('4', new byte[]{56, 36, 34, 127, 32});
-        font.put('5', new byte[]{79, 73, 73, 49, 0});
-        font.put('6', new byte[]{62, 73, 73, 50, 0});
-        font.put('7', new byte[]{3, 1, 1, 127, 0});
-        font.put('8', new byte[]{54, 73, 73, 54, 0});
-        font.put('9', new byte[]{38, 73, 73, 62, 0});
-        font.put('A', new byte[]{0x3C, 0x0A, 0x0A, 0x3C});
-        font.put('B', new byte[]{0x3E, 0x2A, 0x2A, 0x14});
-        font.put('C', new byte[]{0x1C, 0x22, 0x22, 0x14});
-        font.put('D', new byte[]{0x3E, 0x22, 0x22, 0x1C});
-        font.put('E', new byte[]{0x3E, 0x2A, 0x2A});
-        font.put('F', new byte[]{0x3E, 0x0A, 0x0A});
-        font.put('G', new byte[]{0x1C, 0x22, 0x2A, 0x2A});
-        font.put('H', new byte[]{0x3E, 0x08, 0x08, 0x3E});
-        font.put('I', new byte[]{0x3E});
-        font.put('J', new byte[]{0x10, 0x20, 0x20, 0x1E});
-        font.put('K', new byte[]{0x3E, 0x08, 0x14, 0x22});
-        font.put('L', new byte[]{0x3E, 0x20, 0x20});
-        font.put('M', new byte[]{0x3E, 0x04, 0x08, 0x04});
-        font.put('N', new byte[]{0x3E, 0x04, 0x08, 0x3E});
-        font.put('O', new byte[]{0x1C, 0x22, 0x22, 0x1C});
-        font.put('P', new byte[]{0x3E, 0x0A, 0x0A, 0x1C, 0x04});
-        font.put('Q', new byte[]{0x1C, 0x22, 0x12, 0x2C});
-        font.put('R', new byte[]{0x3E, 0x0A, 0x1A, 0x24});
-        font.put('S', new byte[]{0x24, 0x2A, 0x2A, 0x12});
-        font.put('T', new byte[]{0x02, 0x02, 0x3E, 0x02, 0x02});
-        font.put('U', new byte[]{0x1E, 0x20, 0x20, 0x1E});
-        font.put('V', new byte[]{0x06, 0x18, 0x20, 0x18, 0x6});
-        font.put('W', new byte[]{0x1E, 0x20, 0x1E, 0x20, 0x1E});
-        font.put('X', new byte[]{0x36, 0x08, 0x08, 0x36});
-        font.put('Y', new byte[]{0x2E, 0x28, 0x28, 0x1E});
-        font.put('Z', new byte[]{0x32, 0x2A, 0x2A, 0x26});
+        font.put(' ', new Byte[]{0});
+        font.put('+', new Byte[]{24, 126, 126, 24, 0});
+        font.put('-', new Byte[]{24, 24, 24, 24, 0});
+        font.put('0', new Byte[]{62, 65, 65, 62, 0});
+        font.put('1', new Byte[]{0, 66, 127, 64, 0});
+        font.put('2', new Byte[]{98, 81, 73, 70, 0});
+        font.put('3', new Byte[]{34, 65, 73, 54, 0});
+        font.put('4', new Byte[]{56, 36, 34, 127, 32});
+        font.put('5', new Byte[]{79, 73, 73, 49, 0});
+        font.put('6', new Byte[]{62, 73, 73, 50, 0});
+        font.put('7', new Byte[]{3, 1, 1, 127, 0});
+        font.put('8', new Byte[]{54, 73, 73, 54, 0});
+        font.put('9', new Byte[]{38, 73, 73, 62, 0});
+        font.put('A', new Byte[]{0x3C, 0x0A, 0x0A, 0x3C});
+        font.put('B', new Byte[]{0x3E, 0x2A, 0x2A, 0x14});
+        font.put('C', new Byte[]{0x1C, 0x22, 0x22, 0x14});
+        font.put('D', new Byte[]{0x3E, 0x22, 0x22, 0x1C});
+        font.put('E', new Byte[]{0x3E, 0x2A, 0x2A});
+        font.put('F', new Byte[]{0x3E, 0x0A, 0x0A});
+        font.put('G', new Byte[]{0x1C, 0x22, 0x2A, 0x2A});
+        font.put('H', new Byte[]{0x3E, 0x08, 0x08, 0x3E});
+        font.put('I', new Byte[]{0x3E});
+        font.put('J', new Byte[]{0x10, 0x20, 0x20, 0x1E});
+        font.put('K', new Byte[]{0x3E, 0x08, 0x14, 0x22});
+        font.put('L', new Byte[]{0x3E, 0x20, 0x20});
+        font.put('M', new Byte[]{0x3E, 0x04, 0x08, 0x04});
+        font.put('N', new Byte[]{0x3E, 0x04, 0x08, 0x3E});
+        font.put('O', new Byte[]{0x1C, 0x22, 0x22, 0x1C});
+        font.put('P', new Byte[]{0x3E, 0x0A, 0x0A, 0x1C, 0x04});
+        font.put('Q', new Byte[]{0x1C, 0x22, 0x12, 0x2C});
+        font.put('R', new Byte[]{0x3E, 0x0A, 0x1A, 0x24});
+        font.put('S', new Byte[]{0x24, 0x2A, 0x2A, 0x12});
+        font.put('T', new Byte[]{0x02, 0x02, 0x3E, 0x02, 0x02});
+        font.put('U', new Byte[]{0x1E, 0x20, 0x20, 0x1E});
+        font.put('V', new Byte[]{0x06, 0x18, 0x20, 0x18, 0x6});
+        font.put('W', new Byte[]{0x1E, 0x20, 0x1E, 0x20, 0x1E});
+        font.put('X', new Byte[]{0x36, 0x08, 0x08, 0x36});
+        font.put('Y', new Byte[]{0x2E, 0x28, 0x28, 0x1E});
+        font.put('Z', new Byte[]{0x32, 0x2A, 0x2A, 0x26});
     }
 
 
@@ -88,12 +91,12 @@ public class Flipdots {
     }
 
     public void writeText(String params) {
-// FIXME Remove System.out
+        // FIXME Remove System.out
         System.out.println("Flipdots.writeText " + params);
-    }
-
-    private byte[] convertToByteArray(String letters) {
-        return null;
+        List<Byte> bytes = new ArrayList<>();
+        for (char c : params.toCharArray()) {
+            bytes.addAll(Arrays.asList(font.get(c)));
+        }
     }
 
     private void openConnection() {
